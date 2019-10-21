6542ad21df8e4fefb1b7974334361f824302952b
Baruti-1 <barutibaruani1@gmail.com>
2019-10-21 20:14:39 +0300

    Added viewing specific movie files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppMovieComponent } from './app-movie/app-movie.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppMovieComponent
+    AppMovieComponent,
+    MoviesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
